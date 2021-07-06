bb0ac8be00933f9a31bcc2792a95da6314e4f609
mesi473 <meseretkifle2@gmail.com>
2021-07-06 15:46:46 +0300

    footer component is added

--- e_learning/src/components/Login.js
+++ e_learning/src/components/Login.js
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './login.css'
 
 export default function Login() {
+    const [state,setState]=React.useState({
+        username:'',
+        password:''
+    })
     return (
-        <div>
-            login
+        <div className="login-page">
+            <div className="login-area">
+                <div className="log">
+                    <h1>login</h1>
+                    <input
+                    onChange={(e)=>{
+                        setState({...state,username:e.target.value})
+                    }} 
+                    value={state.username} type="text" name="username" placeholder="username"/>
+                    <input onChange={(e)=>{
+                        setState({...state,password:e.target.value})
+                    }} 
+                    value={state.password} type="password" name="password" placeholder="password" required="required"/>
+                    <Link  to="/dashboard" onClick={()=>{
+                        
+                    }}>Login</Link>
+                </div>
+            </div>
         </div>
     )
 }
