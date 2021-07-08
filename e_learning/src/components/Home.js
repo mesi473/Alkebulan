6217138fa67e72604d939b08cbda3dbde01a32e1
mesi473 <meseretkifle2@gmail.com>
2021-07-08 22:31:01 +0300

    register page is added

--- e_learning/src/components/Home.js
+++ e_learning/src/components/Home.js
@@ -5,13 +5,23 @@ import Header from './Header';
 import Footer from './Footer';
 import {Route} from 'react-router-dom'
 import './home.css';
+import Image2 from '../images/PngItem_2253743.png'
 
 export default function Home() {
     return (
-        <div className="home">
+        <div>
             <Header/>
-            <Login/>
-            <Route path="/register" exact component={Register}/>
+            <div className="home">
+                <Route path="/login" exact component={Login}/>
+                <Route path="/register" exact component={Register}/>
+                <div className="home_image">
+                    <img src={Image2} alt=""/>
+                    <h1>We Work for Better Future</h1>
+                </div>
+                <div className="home_image">
+                    <h1>ሀ ሁ ሂ ሃ ሄ ህ ሆ</h1>
+                </div>
+            </div>
             <Footer/>
         </div>
     )
--- e_learning/src/components/Register.js
+++ e_learning/src/components/Register.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './register.css';
+import {Link} from 'react-router-dom'
 
 export default function Register() {
+    const [state,setState]=React.useState({
+        username:'',
+        password:''
+    })
     return (
-        <div className="">
-            <div className="">
-
-            </div>
-            <div>
-
+        <div className="register-page">
+            <div className="register-page-area">
+                <h1>Register</h1>
+                <div className="register-main-page">
+                    <div>
+                        <input type="text" name="username" placeholder="username"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                    </div>
+                    <div>
+                        <input type="text" name="username" placeholder="username"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                    </div>
+                    <div>
+                        <input type="text" name="username" placeholder="username"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                    </div>
+                    <div>
+                        <input type="text" name="username" placeholder="username"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                        <input type="password" name="password" placeholder="password" required="required"/>
+                    </div>
+                </div>
+                <Link  to="/dashboard" onClick={()=>{
+                        
+                }}>Register</Link>
             </div>
         </div>
     )
